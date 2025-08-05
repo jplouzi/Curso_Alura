0962d7c3edf39d00be4ce076118b2477ba54fee2
Joab Pereira Louzi <Joab Louzi>
2025-08-05 19:04:50 -0300

    adicionado projetos do desafio

---
+++ Praticando-com-desafios/sorteador-numeros/app.js
@@ -0,0 +1,63 @@
+ function sortear(){
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);  
+    
+    let sorteados = [];
+    let numero;
+
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('A quantidade de números solicitada é maior que o intervalo disponível. Verifique!');
+        return;
+    }
+
+    for(let i = 0; i < quantidade; i++){
+        numero = sortearNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)){
+            numero = sortearNumeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+
+    }
+
+    console.log (sorteados);
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
+
+}
+
+function sortearNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botaoReiniciar = document.getElementById("btn-reiniciar");
+    
+    if(botaoReiniciar.classList.contains("container__botao-desabilitado")){
+        botaoReiniciar.classList.remove("container__botao-desabilitado");
+        botaoReiniciar.classList.add("container__botao");
+
+    }else{
+        botaoReiniciar.classList.remove("container__botao");
+        botaoReiniciar.classList.add("container__botao-desabilitado");
+
+    }
+}
+
+function reiniciar(){
+    document.getElementById("quantidade").value = '';
+    document.getElementById("de").value = '';
+    document.getElementById("ate").value = '';
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+    alterarStatusBotao();
+}
